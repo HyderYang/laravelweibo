74b29429404f6c64a0b0bd09f7003098e86c283b
hyder <yanghuan0613@gmail.com>
2019-04-28 15:46:05 +0800

    base page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", "StaticPagesController@home");
+Route::get("/help", "StaticPagesController@help");
+Route::get("/about", "StaticPagesController@about");
