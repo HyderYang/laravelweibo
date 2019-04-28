ff0bb1dd2207fdb0974c281fb8e0665e5091d341
hyder <yanghuan0613@gmail.com>
2019-04-28 17:05:20 +0800

    注册页面

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", "StaticPagesController@home")->name('index');
 Route::get("/help", "StaticPagesController@help")->name('help');
 Route::get("/about", "StaticPagesController@about")->name('about');
+
+Route::get("/signup", "usersController@create")->name('signup');
