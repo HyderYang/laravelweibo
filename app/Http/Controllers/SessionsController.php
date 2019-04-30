313ee42444ef1e5cd354cd8855b243c0f0341fd4
hyder <yanghuan0613@gmail.com>
2019-05-01 00:04:11 +0800

    用户退出逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,4 +25,10 @@ class SessionsController extends Controller {
 			return redirect()->back()->withInput();
 		}
 	}
+	
+	public function destroy() {
+		Auth::logout();
+		session()->flash('success', '已经成功推出');
+		return redirect('login');
+	}
 }
