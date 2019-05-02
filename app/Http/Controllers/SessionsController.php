9b86ddbf45961e6e5460b2f70bcf763e72276e76
hyder <yanghuan0613@gmail.com>
2019-05-02 23:07:19 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,9 +24,15 @@ class SessionsController extends Controller {
 		]);
 		
 		if(Auth::attempt($credentials, $request->has('remember'))) {
-			session()->flash('success', '欢迎回来!');
-			$fallback = route('users.show', Auth::user());
-			return redirect()->intended($fallback);
+			if(Auth::user()->activated){
+				session()->flash('success', '欢迎回来!');
+				$fallback = route('users.show', Auth::user());
+				return redirect()->intended($fallback);
+			} else {
+				Auth::logout();
+				session()->flash('warning', '请激活注册邮件');
+				return redirect('/');
+			}
 		} else {
 			session()->flash('danger', '很抱歉, 邮箱密码不匹配');
 			return redirect()->back()->withInput();
