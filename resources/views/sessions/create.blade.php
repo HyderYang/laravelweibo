0dbccd6226ac56fd6b96612973ee71adbd286286
hyder <yanghuan0613@gmail.com>
2019-05-02 23:25:04 +0800

    重置密码

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -2,7 +2,7 @@
 @section('title', '登录')
 
 @section('content')
-	<div class="offset-md-2 col-md-8">
+  <div class="offset-md-2 col-md-8">
   <div class="card ">
     <div class="card-header">
       <h5>登录</h5>
@@ -19,7 +19,7 @@
         </div>
 
         <div class="form-group">
-          <label for="password">密码：</label>
+          <label for="password">密码（<a href="{{ route('password.request') }}">忘记密码</a>）：</label>
           <input type="password" name="password" class="form-control" value="{{ old('password') }}">
         </div>
         
