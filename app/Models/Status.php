4f416dc8b003f90eb4b570a31eb119e3c5116606
hyder <yanghuan0613@gmail.com>
2019-05-03 23:15:06 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
  * @method static insert($toArray)
  */
 class Status extends Model {
+
+	protected $fillable = ['content'];
 	
 	public function user() {
 		return $this->belongsTo(User::class);
