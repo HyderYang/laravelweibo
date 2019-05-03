3b464e928769d8d8bee1314cd33a75a46688e3ad
hyder <yanghuan0613@gmail.com>
2019-05-03 23:46:34 +0800

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.default')
 
 @section('content')
-	@if (Auth::check())
-		<div class="row">
+  @if (Auth::check())
+    <div class="row">
       <div class="col-md-8">
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
@@ -14,8 +17,8 @@
         </section>
       </aside>
     </div>
-	@else
-		<div class="jumbotron">
+  @else
+    <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
         你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
@@ -27,5 +30,5 @@
         <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
       </p>
     </div>
-	@endif
+  @endif
 @stop
